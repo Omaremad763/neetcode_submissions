1967c46d3c6b341ddb1308b91a981670cf5ddc48
Omar Emad Abusaif <78951987+Omaremad763@users.noreply.github.com>
2026-06-18 08:16:33 +0300

    Add: rotate-matrix - submission-0

---
+++ Data Structures & Algorithms/rotate-matrix/submission-0.cs
@@ -0,0 +1,29 @@
+public class Solution {
+    public void Rotate(int[][] matrix) {
+        if (matrix == null || matrix.Length == 0) {
+            return;
+        }
+
+        int n = matrix.Length;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            int left = 0;
+            int right = n - 1;
+            while (left < right) {
+                int temp = matrix[i][left];
+                matrix[i][left] = matrix[i][right];
+                matrix[i][right] = temp;
+                left++;
+                right--;
+            }
+        }
+    }
+}
